5342b400da026e81514e5fc8c889c2cbc4d0f87c
hsw824 <ghdtjddn0824@naver.com>
2022-09-02 08:40:41 +0900

    Feat: Display configuration

---
+++ prepare/front/components/LoginForm.js
@@ -0,0 +1,60 @@
+import { Button, Form, Input } from "antd";
+import Link from "next/link";
+import React, { useCallback } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import useInput from "../hooks/useInput";
+
+const FormWrapepr = styled(Form)`
+  padding: 10px;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const onSubmitForm = useCallback(() => {
+    console.log(id, password);
+    setIsLoggedIn(true);
+  }, [id, password]);
+  return (
+    <FormWrapepr onFinish={onSubmitForm}>
+      <div>
+        <label htmlFor="user-id">아이디</label>
+        <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-id">비밀번호</label>
+        <br />
+        <Input
+          name="user-id"
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <ButtonWrapper>
+        <Button type="primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </ButtonWrapper>
+    </FormWrapepr>
+  );
+};
+
+LoginForm.propTypes = {
+  setIsLoggedIn: PropTypes.func.isRequired,
+};
+
+export default LoginForm;
---
+++ prepare/front/hooks/useInput.js
@@ -0,0 +1,8 @@
+import { useState, useCallback } from "react";
+export default (initialValue = null) => {
+  const [value, setValue] = useState(initialValue);
+  const handler = useCallback((e) => {
+    setValue(e.currentTarget.value);
+  }, []);
+  return [value, handler];
+};
